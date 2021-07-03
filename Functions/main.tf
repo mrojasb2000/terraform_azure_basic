1ee9af482383d6de21358a939ac5166c57a0d717
mavro <mrojasb2000@gmail.com>
2021-07-03 16:58:23 -0400

    Added function app

--- Functions/main.tf
+++ Functions/main.tf
@@ -53,3 +53,30 @@ resource "azurerm_app_service_plan" "app_service_plan" {
     size = "Y1"
   }
 }
+
+# Create an function app
+resource "azurerm_function_app" "function_app" {
+  name = "${var.project}-${var.environment}-fun-app"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = var.location
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "",
+    "FUNCTIONS_WORKER_RUNTIME" = "node",
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.application_insights.instrumentation_key,
+  }
+  os_type = "linux"
+  site_config {
+    linux_fx_version = "node|14"
+    use_32_bit_worker_process = false
+  }
+  storage_account_name = azurerm_storage_account.storage_account.name
+  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
+  version = "~3"
+
+  lifecycle {
+    ignore_changes = [
+      app_settings["WEBSITE_RUN_FROM_PACKAGE"],
+    ]
+  }
+}
